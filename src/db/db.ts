86e8b40566117905fce37b422f6df2051c10f91f
Amaan Patel <amaanp32@gmail.com>
2025-11-12 17:11:00 +0530

    added db connection

---
+++ src/db/db.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGO_URI!;
+
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env"
+  );
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function connectToDb() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    mongoose.connect(MONGODB_URI).then(() => mongoose.connection);
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    cached.promise = null;
+    throw error;
+  }
+
+  return cached.conn;
+}
